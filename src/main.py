f15c339ac00eb88c2bc556215c5f744e454f5680
redstonesb <scbakken89@gmail.com>
2020-02-28 22:42:35 -0800

    Hopfully working nuron

--- src/main.py
+++ src/main.py
@@ -3,12 +3,10 @@ import numpy as np
 def signoid(x):
     return 1 / (1 + np.exp(-x))
 
-training_inputs = np.array([
-                           [0,0,1]
-                           [1,1,1]
-                           [1,0,1]
-                           [0,1,1]
-                                 ])
+training_inputs = np.array([[0,0,1],
+                            [1,1,1],
+                            [1,0,1],
+                            [0,1,1]])
 
 training_outputs = np.array([[0,1,1,0]]).T
 
@@ -18,3 +16,12 @@ synaptic_weights = 2 * np.random((3,1))-1
 
 print('random starting weights: ')
 print(synaptic_weights)
+
+for iteration in range(1):
+
+    input_layer = training_inputs
+
+    outputs = sigmoid(np.dot(input_layer, synaptic_weights))
+
+print('Outputs after training: ')
+print(outputs)

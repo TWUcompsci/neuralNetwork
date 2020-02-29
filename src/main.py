2550a697bca6cd186ab44d1306ced8b8b109ff7e
redstonesb <scbakken89@gmail.com>
2020-02-28 22:35:34 -0800

    writing...

--- src/main.py
+++ src/main.py
@@ -2,3 +2,19 @@ import numpy as np
 
 def signoid(x):
     return 1 / (1 + np.exp(-x))
+
+training_inputs = np.array([
+                           [0,0,1]
+                           [1,1,1]
+                           [1,0,1]
+                           [0,1,1]
+                                 ])
+
+training_outputs = np.array([[0,1,1,0]]).T
+
+np.random.seed(1)
+
+synaptic_weights = 2 * np.random((3,1))-1
+
+print('random starting weights: ')
+print(synaptic_weights)

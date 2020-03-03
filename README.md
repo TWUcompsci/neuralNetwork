2aafe309c668e6a444557771d735af75e7214de9
faitht1023 <43914867+faitht1023@users.noreply.github.com>
2020-03-02 23:27:26 -0800

    added to the code and edited readme

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ Simple neural network using python
 
 ### Follow this tutorial!
 <https://www.youtube.com/watch?v=kft1AJ9WVDk>
+
+##### Training Process
+1. Take the inputs from the training example and put them through our formula to get the neuron's output
+2. Calculate the error, which is the difference between the output we got and the actual output
+3. Depending on the severeness of the error, adjust the weights accordingly
+4. Repeat the process 20,000 times!
+
+!! Sigmoid Normalizing Function
+!! phi(x) = 1 / (1 + e^(-x))
+
+! Adjusting Weights (Error Weight Derivative)
+! Use error.input.phi'(output)
+! error = output - actual output
+! input = 1 or 0
+! phi'(x) = x
--- src/main.py
+++ src/main.py
@@ -1,27 +1,54 @@
 import numpy as np
 
-def signoid(x):
+#sigmoid normalizing function
+def sigmoid(x):
+    #returns any value between 0 and 1
     return 1 / (1 + np.exp(-x))
 
+def sigmoid_derivative(x):
+    #take the derivative of the above function and this results...
+    return x + (1 - x)
+
+#defining the training inputs
 training_inputs = np.array([[0,0,1],
                             [1,1,1],
                             [1,0,1],
                             [0,1,1]])
 
+#".T" tranposes the matrix so it's 4x1
 training_outputs = np.array([[0,1,1,0]]).T
 
+#getting random numbers
 np.random.seed(1)
 
-synaptic_weights = 2 * np.random((3,1))-1
+#creating a 3x1 matrix of random values
+synaptic_weights = 2 * np.random.random((3,1)) - 1
 
-print('random starting weights: ')
-print(synaptic_weights)
+#displaying our weights
+print("Random starting synaptic weights:", synaptic_weights)
 
-for iteration in range(1):
+#iterate 20000 times...this is some dang rigorous training!
+#more iterations = less error
+for iteration in range(20000):
 
     input_layer = training_inputs
 
+    #taking the weighted sum using the dot product
     outputs = sigmoid(np.dot(input_layer, synaptic_weights))
 
-print('Outputs after training: ')
-print(outputs)
+    #calculate the error
+    error = training_outputs - outputs
+
+    #calculate the adjustments
+    adjustments = error + sigmoid_derivative(outputs)
+
+    #calculate the synaptic_weights by taking the dot product
+    #of the transpose of input_layer and adjustments
+    #take the transpose, otherwise you literally can't dot it
+    synaptic_weights += np.dot(input_layer.T, adjustments)
+
+#display synaptic weights
+print("Synaptic weights after training:", synaptic_weights)
+
+#display the outputs
+print('Outputs after training: ', outputs)
